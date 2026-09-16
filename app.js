9a9d0c4a739c131d83ef58b66f2823e113fe5dd4
JuanLeonI <Juandanielleon22@gmail.com>
2026-09-15 19:10:27 -0700

    aa

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Número de teléfono de tu negocio (formato internacional sin signos ni espacios)
-const TELEFONO_WHATSAPP = "52662420369";
+const TELEFONO_WHATSAPP = "662420369";
 
 // 1. Catálogo de perfumes
 const productos = [
